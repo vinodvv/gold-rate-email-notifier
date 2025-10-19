a80cbd1068fc6e52854904b48340f575928bae96
Vinod VV <vinovijayan@gmail.com>
2025-10-19 23:08:56 +0530

    Enhanced to send email using smtplib, renamed fetch_rates.py to fetch_rates_and_send_email.py

--- fetch_rates_and_send_email.py
+++ fetch_rates_and_send_email.py
@@ -3,20 +3,31 @@ from dotenv import load_dotenv
 from datetime import datetime
 import csv
 import os
+import smtplib
+from email.message import EmailMessage
+
 
 # Constants
 CSV_FILE = "rates.csv"
 TROY_OZ_GRAMS = 31.1035
 GOLD_RATIOS = {"24k": 1.0, "22k": 0.916, "18k": 0.750}
+INR_SYMBOL = "\u20B9"
 
 
 def load_credentials():
-    """Load API key from .env file."""
+    """Load API key, email credentials  from a .env file."""
     load_dotenv(override=True)
     api_key = os.getenv("API_KEY")
-    if api_key is None:
-        raise ValueError("API_KEY not found in .env file")
-    return api_key
+    email_address = os.getenv("EMAIL_ADDRESS")
+    email_password = os.getenv("EMAIL_PASSWORD")
+
+    missing = []
+    if not api_key: missing.append("API_KEY")
+    if not email_address: missing.append("EMAIL_ADDRESS")
+    if not email_password: missing.append("EMAIL_PASSWORD")
+    if missing:
+        raise ValueError("Missing variables in .env file: " + ", ".join(missing))
+    return api_key, email_address, email_password
 
 
 def get_gold_silver_rate(api_key):
@@ -74,16 +85,65 @@ def save_gold_silver_rates(filepath, date, time, prices):
         ])
 
 
+def login_to_email(email_address, email_password):
+    """Log in to the Gmail SMTP server"""
+    try:
+        smtp = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp.starttls()
+        smtp.login(email_address, email_password)
+        return smtp
+    except Exception as e:
+        raise ConnectionError("SMTP login failed: " + str(e))
+
+
+def create_email(prices, sender_name, recipient, recipient_name):
+    """Create an email"""
+    msg = EmailMessage()
+    msg["Subject"] = f"🪙 Today's Gold, Silver Prices"
+    msg["From"] = sender_name
+    msg["To"] = recipient
+
+    # Plain text
+    text_content = (
+        f"Hi {recipient_name},\n\n"
+        f"Today's Gold, Silver Prices:\n\n"
+        f"1. GOLD\n"
+        f"  (a) Gold (24k per Gram): {INR_SYMBOL} {prices['gold_24k_per_gram']:.2f}\n"
+        f"  (b) Gold (22k per Gram): {INR_SYMBOL} {prices['gold_22k_per_gram']:.2f}\n"
+        f"  (c) Gold (18k per Gram): {INR_SYMBOL} {prices['gold_18k_per_gram']:.2f}\n\n"
+        f"2. SILVER\n"
+        f"  (a) Silver (per Gram): {INR_SYMBOL} {prices['silver_per_gram']:.2f}\n"
+        f"  (b) Silver (per Kg): {INR_SYMBOL} {prices['silver_per_kg']:.2f}\n\n"
+        f"Regards,\n{sender_name}"
+    )
+
+    msg.set_content(text_content)
+
+    return msg
+
+
 def main():
-    api_key = load_credentials()
+    api_key, email_address, email_password = load_credentials()
+    sender_name = os.getenv("SENDER_NAME", email_address)
+    recipient = os.getenv("RECIPIENT")
+    recipient_name = os.getenv("RECIPIENT_NAME")
     rates, date, time = get_gold_silver_rate(api_key)
 
     gold_price = rates['INRXAU']
     silver_price = rates['INRXAG']
 
     prices = compute_prices(gold_price, silver_price)
+
     save_gold_silver_rates(CSV_FILE, date, time, prices)
-    print(f"Gold, Silver rates as on {date} saved successfully!")
+
+    try:
+        smtp = login_to_email(email_address, email_password)
+        msg = create_email(prices, sender_name, recipient, recipient_name)
+        smtp.send_message(msg)
+        print("Email sent successfully!")
+        smtp.quit()
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 
 
 if __name__ == "__main__":
